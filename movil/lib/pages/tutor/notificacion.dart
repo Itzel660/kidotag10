7b0d7fb93e2b3e232f65fae5b8a4014e60a62453
HectorVazquez2406 <a22300122@unideh.edu.mx>
2026-03-08 19:23:25 -0600

    Refactor NotificacionesTutor to StatefulWidget

--- movil/lib/pages/tutor/notificacion.dart
+++ movil/lib/pages/tutor/notificacion.dart
@@ -1,19 +1,66 @@
 import 'package:flutter/material.dart';
+import '../../data/notificaciones.dart';
+import 'detalle_notificacion.dart';
 
-class NotificacionesTutor extends StatelessWidget {
+class NotificacionesTutor extends StatefulWidget {
   const NotificacionesTutor({super.key});
 
   @override
+  State<NotificacionesTutor> createState() => _NotificacionesTutorState();
+}
+
+class _NotificacionesTutorState extends State<NotificacionesTutor> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Notificaciones'),
-      ),
-      body: const Center(
-        child: Text(
-          "Pendiente conexión DB",
-          style: TextStyle(fontSize: 16, color: Colors.grey),
-        ),
+      appBar: AppBar(title: const Text("Notificaciones")),
+
+      body: ValueListenableBuilder(
+        valueListenable: notificacionesDB,
+        builder: (context, lista, _) {
+          return ListView.builder(
+            padding: const EdgeInsets.all(12),
+            itemCount: lista.length,
+            itemBuilder: (context, index) {
+              final noti = lista[index];
+
+              return GestureDetector(
+                onTap: () {
+                  final nuevaLista = [...notificacionesDB.value];
+                  nuevaLista[index]["leido"] = true;
+                  notificacionesDB.value = nuevaLista;
+
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) =>
+                          DetalleNotificacion(noti: nuevaLista[index]),
+                    ),
+                  );
+                },
+
+                child: Card(
+                  color: noti["leido"]
+                      ? Colors.white
+                      : Colors.blue.shade50,
+                  elevation: 4,
+                  margin: const EdgeInsets.only(bottom: 12),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: ListTile(
+                    title: Text(
+                      noti["titulo"],
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(noti["mensaje"]),
+                    trailing: Text(noti["fecha"]),
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
